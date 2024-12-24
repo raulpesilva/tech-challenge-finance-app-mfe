f093b222bcf4c816d521c8514a48edab0ba30155
FehSouza <fernanda.souza142010@gmail.com>
2024-12-24 08:32:26 -0300

    refactor(Footer): separation of links into objects outside the component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,9 @@
 import Instagram from "@/assets/icons/instagram-icon.svg";
 import Whatsapp from "@/assets/icons/whatsapp-icon.svg";
 import YouTube from "@/assets/icons/youtube-icon.svg";
-import { combaneStyles } from "@/utils/combaneStyles";
 import Link from "next/link";
 import { LogoType } from "../LogoType";
+import { Typography } from "../shared/Typography";
 import styles from "./styles.module.scss";
 
 interface ExternalLinkProps {
@@ -19,42 +19,65 @@ const ExternalLink = ({ href, children }: ExternalLinkProps) => {
   );
 };
 
+// TODO: Add links in the footer - Services
+const services = [
+  { link: "/", text: "Conta corrente" },
+  { link: "/", text: "Conta PJ" },
+  { link: "/", text: "Cartão de crédito" },
+];
+
+const contacts = [
+  { link: "tel:080000425008", text: "0800 004 250 08" },
+  { link: "mailto:meajuda@bytebank.com.br", text: "meajuda@bytebank.com.br" },
+  {
+    link: "mailto:ouvidoria@bytebank.com.br",
+    text: "ouvidoria@bytebank.com.br",
+  },
+];
+
+const socialMedia = [
+  { link: "https://www.instagram.com/", icon: Instagram },
+  { link: "https://www.whatsapp.com/", icon: Whatsapp },
+  { link: "https://www.youtube.com/", icon: YouTube },
+];
+
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div className={styles.content}>
-          <h2>Serviços</h2>
-          <Link href="/">Conta corrente</Link>
-          <Link href="/">Conta PJ</Link>
-          <Link href="/">Cartão de crédito</Link>
+          <Typography variant="heading4" color="secondary">
+            Serviços
+          </Typography>
+          {services.map((service, i) => (
+            <Link key={`service-${i}`} href={service.link}>
+              {service.text}
+            </Link>
+          ))}
         </div>
 
         <div className={styles.content}>
-          <h2>Contato</h2>
-          <ExternalLink href="tel:080000425008">0800 004 250 08</ExternalLink>
-          <ExternalLink href="mailto:meajuda@bytebank.com.br">
-            meajuda@bytebank.com.br
-          </ExternalLink>
-          <ExternalLink href="mailto:ouvidoria@bytebank.com.br">
-            ouvidoria@bytebank.com.br
-          </ExternalLink>
+          <Typography variant="heading4" color="secondary">
+            Contato
+          </Typography>
+          {contacts.map((contact, i) => (
+            <ExternalLink key={`contact-${i}`} href={contact.link}>
+              {contact.text}
+            </ExternalLink>
+          ))}
         </div>
 
-        <div className={combaneStyles([styles.content, styles.content3])}>
-          <h2>Desenvolvido por Alura</h2>
+        <div className={styles.content}>
+          <Typography variant="heading4" color="secondary">
+            Desenvolvido por Alura
+          </Typography>
           <LogoType />
-
           <div className={styles.socialMediaWrapper}>
-            <ExternalLink href="https://www.instagram.com/">
-              <Instagram />
-            </ExternalLink>
-            <ExternalLink href="https://www.whatsapp.com/">
-              <Whatsapp />
-            </ExternalLink>
-            <ExternalLink href="https://www.youtube.com/">
-              <YouTube />
-            </ExternalLink>
+            {socialMedia.map((media, i) => (
+              <ExternalLink key={`media-${i}`} href={media.link}>
+                <media.icon />
+              </ExternalLink>
+            ))}
           </div>
         </div>
       </div>
