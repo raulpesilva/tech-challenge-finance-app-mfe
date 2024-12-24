b412c3aa48d5f743bfb0bcba17beb1fca5630fe2
FehSouza <fernanda.souza142010@gmail.com>
2024-12-24 17:54:52 -0300

    feat: add button shared component

--- src/components/shared/Button/index.tsx
+++ src/components/shared/Button/index.tsx
@@ -1,14 +1,33 @@
-import styles from './styles.module.scss';
+import Loading from "../../../assets/icons/loading-icon.svg";
+import styles from "./styles.module.scss";
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant: "outlined" | "contained";
+  color: "primary" | "secondary" | "tertiary" | "error" | "cta";
   children?: React.ReactNode;
+  loading?: boolean;
   onClick?: () => void;
 }
 
-export const Button = ({ children, ...props }: ButtonProps) => {
+export const Button = ({
+  variant,
+  color,
+  children,
+  loading,
+  ...props
+}: ButtonProps) => {
   return (
-    <button className={styles.button} {...props}>
-      {children}
+    <button
+      className={[
+        styles.button,
+        styles[variant],
+        styles[color],
+        loading && styles.loading,
+      ].join(" ")}
+      {...props}
+    >
+      {!loading && children}
+      {!!loading && <Loading />}
     </button>
   );
 };
