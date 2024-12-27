18e5ad057311e816cc7ab935813a78994deee1fa
FehSouza <fernanda.souza142010@gmail.com>
2024-12-27 17:59:03 -0300

    feat(Select): add Select shared component

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,27 @@
+"use client";
+
+import { Select } from "@/components/shared/Select";
+import { useState } from "react";
+import styles from './styles.module.scss'
+
 export default function Page() {
+  const statusOptions = ["Pendente", "Em andamento", "Concluída"] as const;
+  const [status, setStatus] = useState<(typeof statusOptions)[number] | null>(
+    null
+  );
+
   return (
-    <div>
+    <div className={styles.container}>
       <h2>Home</h2>
       <p>Welcome to the home</p>
+
+      <Select
+        label="Status"
+        placeholder="Selecione uma opção"
+        options={statusOptions}
+        value={status}
+        onChange={setStatus}
+      />
     </div>
   );
 }
