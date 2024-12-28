dc1ccd7fec48e95d62632e5df545bad9021d5b6b
FehSouza <fernanda.souza142010@gmail.com>
2024-12-28 18:32:01 -0300

    feat(Header): add Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,20 @@
-import Link from 'next/link';
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import { LogoType } from "../LogoType";
 
 export const Header = () => {
   return (
-    <header style={{ gap: 8, display: 'flex', flexDirection: 'column' }}>
-      <h1>Home header</h1>
-      <div style={{ gap: 8, display: 'flex' }}>
-        <Link href="/">Home</Link>
-        <Link href="/login">Login</Link>
-        <Link href="/register">Register</Link>
-        <Link href="/dashboard">dashboard</Link>
-        <Link href="/noexiste">noexiste</Link>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <LogoType />
+
+        <div>
+          <Link href="/">Home</Link>
+          <Link href="/login">Login</Link>
+          <Link href="/register">Register</Link>
+          <Link href="/dashboard">dashboard</Link>
+          <Link href="/noexiste">noexiste</Link>
+        </div>
       </div>
     </header>
   );
