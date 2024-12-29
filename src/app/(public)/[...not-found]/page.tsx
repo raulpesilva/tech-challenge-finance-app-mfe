9dd23301fb4dd4d679dc0d513fa62b6a96f93080
FehSouza <fernanda.souza142010@gmail.com>
2024-12-29 15:56:43 -0300

    feat(Not Found): add not found page - public and dashboard

--- src/app/(public)/[...not-found]/page.tsx
+++ src/app/(public)/[...not-found]/page.tsx
@@ -1,5 +1,4 @@
-import { ButtonLink } from "@/components/shared/ButtonLink";
-import { Typography } from "@/components/shared/Typography";
+import { NotFoundContainer } from "@/components/NotFoundContainer";
 import { headers } from "next/headers";
 import Page from "../page";
 import styles from "./styles.module.scss";
@@ -13,19 +12,8 @@ export default async function NotFound() {
   if (publicRoutes.some((route) => pathname?.includes(route))) return <Page />;
 
   return (
-    <main className={styles.main}>
-      <Typography variant="heading1">
-        Ops! Não encontramos a página...{" "}
-      </Typography>
-
-      <Typography variant="paragraph">
-        E olha que exploramos o universo procurando por ela! Que tal voltar e
-        tentar novamente?
-      </Typography>
-
-      <ButtonLink href="/" variant="contained" color="secondary">
-        Voltar ao início
-      </ButtonLink>
+    <main className={styles.notFoundMain}>
+      <NotFoundContainer />
     </main>
   );
 }
