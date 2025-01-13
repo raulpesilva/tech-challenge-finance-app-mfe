2758767a65adb84630a79fe3cb782f812e1ac5f6
raulpesilva <adraulpesilva@gmail.com>
2025-01-13 02:45:37 -0300

    feat: enhance currency masking to support negative values

--- src/utils/masks/maskCurrency/index.ts
+++ src/utils/masks/maskCurrency/index.ts
@@ -1,11 +1,14 @@
 export const maskCurrency = (value: string |number| undefined ) => {
   if(!value) return '';
   value = String(value);
+  let signal = '';
+  if(value.startsWith('-')) signal = '-';
   value = value.replace(/\D/g, '');
   value = value.replace(/^0+(?!$)/, '');
   value = value.padStart(3, '0');
   value = value.replace(/(\d+)(\d{2})$/, '$1,$2');
   value = value.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+  value = signal + value;
   return value;
 };
 
